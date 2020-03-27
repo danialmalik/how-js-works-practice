7203b17cc0ff86f0163804fa92e1d12d435eceb9
danialmalik <danialmalik321@gmail.com>
2020-03-27 13:12:57 +0500

    how big integers work: BigInteger lib

--- 2-how-big-numbers-work/bigInteger.js
+++ 2-how-big-numbers-work/bigInteger.js
@@ -6,6 +6,15 @@
  * ["+", 8650752, 7098594, 31974] is 900000000000000000000 because
  * 900000000000000000000 = 8650752 + ((7098594 + (31974 * 16777216)) * 16777216)
  */
+
+/*property
+   abs, abs_lt, add, and, clz32, concat, create, div, divrem, eq, every, fill,
+   floor, forEach, freeze, gcd, isArray, isInteger, isSafeInteger,
+   is_big_integer, is_negative, is_positive, is_zero, join, length, lt, make,
+   map, mask, mul, neg, not, number, or, population, power, push, random,
+   reduce, reverse, shift_down, shift_up, significant_bits, signum, slice,
+   split, string, sub, ten, toUpperCase, two, wun, xor, zero
+*/
 const radix = 16777216;
 const radix_squared = radix ** 2;
 const log2_radix = 24;
@@ -527,3 +536,396 @@ function mul(multiplicand, multiplier) {
     });
     return mint(result);
 }
+
+
+/**
+ * Divide and return quotient as well as the remainder
+ */
+function divrem(dividend, divisor) {
+    if (is_zero(dividend) || abs_lt(dividend, divisor)) {
+        return [zero, dividend];
+    }
+    if (is_zero(divisor)) {
+        return undefined;
+    }
+
+    // Make the operands positive.
+
+    let quotient_is_negative = dividend[sign] !== divisor[sign];
+    let remainder_is_negative = dividend[sign] === minus;
+    let remainder = dividend;
+    dividend = abs(dividend);
+    divisor = abs(divisor);
+
+    // We estimate the next digit of the quotient. We subtract the divisor times that
+    // estimate from the dividend, and then we go again. We are using base 16777216
+    // instead of base 10, and we are being more systematic in predicting
+    // the next digit of the quotent.
+
+    // In order to improve our predictions, we first mint the divisor. We shift it
+    // left until its most significant bit is '1'. We also shift the dividend by
+    // the same amount.
+
+    // To determine the shift count, we find the number of leading zero bits.
+    // The 'clz32' function counts in a field of 32 bits, but we are only
+    // concerned with a field of 24 bits, so we subtract 8.
+
+    let shift = Math.clz32(last(divisor)) - 8;
+
+    dividend = shift_up(dividend, shift);
+    divisor = shift_up(divisor, shift);
+    let place = dividend.length - divisor.length;
+    let dividend_prefix = last(dividend);
+    let divisor_prefix = last(divisor);
+    if (dividend_prefix < divisor_prefix) {
+        dividend_prefix = (dividend_prefix * radix) + next_to_last(dividend);
+    } else {
+        place += 1;
+    }
+    divisor = shift_up(divisor, (place - 1) * 24);
+    let quotient = new Array(place + 1).fill(0);
+    quotient[sign] = plus;
+    while (true) {
+
+        // The estimate will not be too small, but it might be too large. If it is
+        // too large then subtracting the product of the estimate and the divisor
+        // from the dividend produces a negative. When that happens, make the
+        // estimate smaller and try again.
+
+        let estimated = Math.floor(dividend_prefix / divisor_prefix);
+        if (estimated > 0) {
+            while (true) {
+                let trial = sub(dividend, mul(divisor, [plus, estimated]));
+                if (!is_negative(trial)) {
+                    dividend = trial;
+                    break;
+                }
+                estimated -= 1;
+            }
+        }
+
+        // The corrected estimate is stored in the 'quotient'.
+        // If that was the final place, then move on.
+
+        quotient[place] = estimated;
+        place -= 1;
+        if (place === 0) {
+            break;
+        }
+
+        // Prepare for the next place. Update 'dividend_prefix' with the first two
+        // words of the remaining 'dividend', and scale down the 'divisor'.
+
+        if (is_zero(dividend)) {
+            break;
+        }
+        dividend_prefix = last(dividend) * radix + next_to_last(dividend);
+        divisor = shift_down(divisor, 24);
+    }
+
+    // Fix the remainder.
+
+    quotient = mint(quotient);
+    remainder = shift_down(dividend, shift);
+    return [
+        (
+            quotient_is_negative
+                ? neg(quotient)
+                : quotient
+        ),
+        (
+            remainder_is_negative
+                ? neg(remainder)
+                : remainder
+        )
+    ];
+}
+
+/**
+ * Divide but return only the quotient
+ */
+function div(dividend, divisor) {
+    let temp = divrem(dividend, divisor);
+    if (temp) {
+        return temp[0];
+    }
+}
+
+function power(big, exponent) {
+    let exp = int(exponent);
+    if (exp === 0) {
+        return wun;
+    }
+    if (is_zero(big)) {
+        return zero;
+    }
+
+    // only supporting positive exponents
+    if (exp === undefined || exp < 0) {
+        return undefined;
+    }
+
+    let result = wun;
+    while (true) {
+        if ((exp & 1) !== 0) {
+            result = mul(result, big);
+        }
+        exp = Math.floor(exp / 2);
+        if (exp < 1) {
+            break;
+        }
+        big = mul(big, big);
+    }
+    return mint(result);
+}
+
+/**
+ * Reduce the fractions
+ */
+function gcd(a, b) {
+    a = abs(a);
+    b = abs(b);
+    while (!is_zero(b)) {
+        let [ignore, remainder] = divrem(a, b);
+        a = b;
+        b = remainder;
+    }
+    return a;
+}
+
+/**
+ * For to and from convertions of numbers and strings
+ */
+const digitset = "0123456789ABCDEFGHJKMNPQRSTVWXYZ*~$=U";
+const charset = (function (object) {
+    digitset.split("").forEach(function (element, element_nr) {
+        object[element] = element_nr;
+    });
+    return Object.freeze(object);
+}(Object.create(null)));
+
+
+// The 'make' function returns a big integer. The value parameter is
+// a string and an optional radix, or an integer, or a big_integer.
+
+function make(value, radix_2_37) {
+
+
+    let result;
+    if (typeof value === "string") {
+        let radish;
+        if (radix_2_37 === undefined) {
+            radix_2_37 = 10;
+            radish = ten;
+        } else {
+            if (
+                !Number.isInteger(radix_2_37)
+                || radix_2_37 < 2
+                || radix_2_37 > 37
+            ) {
+                return undefined;
+            }
+            radish = make(radix_2_37);
+        }
+        result = zero;
+        let good = false;
+        let negative = false;
+        if (value.toUpperCase().split("").every(
+            function (element, element_nr) {
+                let digit = charset[element];
+                if (digit !== undefined && digit < radix_2_37) {
+                    result = add(mul(result, radish), [plus, digit]);
+                    good = true;
+                    return true;
+                }
+                if (element_nr === sign) {
+                    if (element === plus) {
+                        return true;
+                    }
+                    if (element === minus) {
+                        negative = true;
+                        return true;
+                    }
+                }
+                return digit === "_";
+            }
+        ) && good) {
+            if (negative) {
+                result = neg(result);
+            }
+            return mint(result);
+        }
+        return undefined;
+    }
+    if (Number.isInteger(value)) {
+        let whole = Math.abs(value);
+        result = [(
+            value < 0
+                ? minus
+                : plus
+        )];
+        while (whole >= radix) {
+            let quotient = Math.floor(whole / radix);
+            result.push(whole - (quotient * radix));
+            whole = quotient;
+        }
+        if (whole > 0) {
+            result.push(whole);
+        }
+        return mint(result);
+    }
+    if (Array.isArray(value)) {
+        return mint(value);
+    }
+}
+
+
+function number(big) {
+    let value = 0;
+    let the_sign = 1;
+    let factor = 1;
+    big.forEach(function (element, element_nr) {
+        if (element_nr === 0) {
+            if (element === minus) {
+                the_sign = -1;
+            }
+        } else {
+            value += element * factor;
+            factor *= radix;
+        }
+    });
+    return the_sign * value;
+}
+
+/**
+ * Convert a big integer to string
+ */
+function string(a, radix_2_thru_37 = 10) {
+    if (is_zero(a)) {
+        return "0";
+    }
+    radix_2_thru_37 = int(radix_2_thru_37);
+    if (
+        !Number.isSafeInteger(radix_2_thru_37)
+        || radix_2_thru_37 < 2
+        || radix_2_thru_37 > 37
+    ) {
+        return undefined;
+    }
+    const radish = make(radix_2_thru_37);
+    const the_sign = (
+        a[sign] === minus
+            ? "-"
+            : ""
+    );
+    a = abs(a);
+    let digits = [];
+    while (!is_zero(a)) {
+        let [quotient, remainder] = divrem(a, radish);
+        digits.push(digitset[number(remainder)]);
+        a = quotient;
+    }
+    digits.push(the_sign);
+    return digits.reverse().join("");
+}
+
+function population_32(int32) {
+
+    // Produce the total count of '1' bits in a 32 bit integer.
+
+    // Count 16 pairs of bits, producing 16 two bit counts (0, 1, or 2).
+    // For each pair, we subtract the higher bit from the pair,
+    // which converts the two bits into a count.
+    //.                     HL - H = count
+    //.                     00 - 0 = 00
+    //.                     01 - 0 = 01
+    //.                     10 - 1 = 01
+    //.                     11 - 1 = 10
+
+    int32 -= (int32 >>> 1) & 0x55555555;
+
+    // Combine 8 pairs of two bit counts, producing 8 four bit counts,
+    // ranging from 0 thru 4.
+
+    int32 = (int32 & 0x33333333) + ((int32 >>> 2) & 0x33333333);
+
+    // Combine 4 pairs of four bit counts, producing 4 eight bit counts, ranging
+    // from 0 thru 8. Overflow into neighbor counts is no longer possible, so we
+    // only need a single masking operation after the addition.
+
+    int32 = (int32 + (int32 >>> 4)) & 0x0F0F0F0F;
+
+    // Combine 2 pairs of eight bit counts, producing 2 sixteen bit counts,
+    // ranging from 0 thru 16.
+
+    int32 = (int32 + (int32 >>> 8)) & 0x001F001F;
+
+    // Finally, combine the 2 sixteen bit counts,
+    // producing a number ranging number from 0 thru 32.
+
+    return (int32 + (int32 >>> 16)) & 0x0000003F;
+}
+
+function population(big) {
+
+    // Count the total number of '1' bits.
+
+    return big.reduce(
+        function (reduction, element, element_nr) {
+            return reduction + (
+                element_nr === sign
+                    ? 0
+                    : population_32(element)
+            );
+        },
+        0
+    );
+}
+
+function significant_bits(big) {
+
+    // Count the total number of bits excluding leading zeros.
+
+    return (
+        big.length > 1
+            ? make((big.length - 2) * log2_radix + (32 - Math.clz32(last(big))))
+            : zero
+    );
+}
+
+export default Object.freeze({
+    abs,
+    abs_lt,
+    add,
+    and,
+    div,
+    divrem,
+    eq,
+    gcd,
+    is_big_integer,
+    is_negative,
+    is_positive,
+    is_zero,
+    lt,
+    make,
+    mask,
+    mul,
+    neg,
+    not,
+    number,
+    or,
+    population,
+    power,
+    random,
+    shift_down,
+    shift_up,
+    significant_bits,
+    signum,
+    string,
+    sub,
+    ten,
+    two,
+    wun,
+    xor,
+    zero
+});
